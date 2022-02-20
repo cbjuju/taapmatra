e1d086d652896d8e0d51596551844b3ea22d05e9
Sudip Kunda <sudipkunda@gmail.com>
2022-02-20 09:40:02 -0600

    Use argparser to get usage arguments from the user

--- taapmatra.py
+++ taapmatra.py
@@ -1,6 +1,24 @@
 from configparser import ConfigParser
+import argparse
+
+BASE_WEATHER_API_URL = "http://api.openweathermap.org/data/2.5/weather"
 
 def _get_api_key():
+
     config = ConfigParser()
     config.read("secrets.ini")
     return config["openweather"]["api_key"]
+
+def get_args_from_user():
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("city", nargs="+", default=None, help="enter city name")
+
+    parser.add_argument("-i", "--imperial", action="store_true",
+            help="display the temperature in imperial units")
+
+    return parser.parse_args()
+
+if __name__ == "__main__":
+    user_args = get_args_from_user()
